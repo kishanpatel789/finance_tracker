802c8f30591cb89db46d4bb93684b55eeb8fff5b
Kishan Patel <kishanpatel789@gmail.com>
2025-08-01 15:51:59 -0500

    Link API report to Plotly bullet charts

--- src/gui/pages/home.py
+++ src/gui/pages/home.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 from nicegui import ui
 
+from ..helpers import call_api, currency_str_to_float
 from ..theme import theme
 
 
@@ -8,9 +9,6 @@ def create() -> None:
     @ui.page("/")
     def home():
         with theme.frame():
-            ui.label("Finance Tracker").classes("text-xl")
-            ui.label("Placeholder for home dashboard")
-
             mint_green = "#78c2ad"
             mint_red = "#e25c5c"
 
@@ -24,9 +22,9 @@ def create() -> None:
 
                 fig.add_trace(
                     go.Indicator(
-                        mode="gauge",
+                        mode="gauge+number",
                         value=value,
-                        domain={"x": [0.1, 1], "y": y_domain},
+                        domain={"x": [0.2, 1], "y": y_domain},
                         title={
                             "text": f"<b>{title}</b><br><span style='color: gray; font-size:0.8em'>{delta_text}</span>",
                             "font": {"size": 14},
@@ -39,30 +37,45 @@ def create() -> None:
                             },
                             "bar": {"color": bar_color, "thickness": 1},
                         },
+                        number={
+                            "font": {
+                                "size": 20,
+                            },
+                            "prefix": "$",
+                            "valueformat": ",.2f",
+                        },
                         customdata=[[value_text]],
                     )
                 )
 
-                # bar annotation - does not work
-                y_center = (y_domain[0] + y_domain[1]) / 2
-                x_pos = 0.12 + (value / max(value, budget))
-                fig.add_annotation(
-                    x=x_pos,
-                    y=y_center,
-                    text=value_text,
-                    showarrow=False,
-                    font=dict(color="white", size=12),
-                    xanchor="left",
-                    yanchor="middle",
+            @ui.refreshable
+            def report_div():
+                fig = go.Figure()
+
+                result = call_api(
+                    "reports/monthly_budget?year_month=2025-07", method="GET"
                 )
+                num_lines = len(result.data)
+                spacing = 0.01
+                for i, line in enumerate(result.data):
+                    make_bullet(
+                        fig,
+                        line["category_name"],
+                        value=currency_str_to_float(line["amount_spent"]),
+                        budget=currency_str_to_float(line["budget"]),
+                        y_domain=[
+                            (num_lines - i - 1) / num_lines + spacing,
+                            (num_lines - i) / num_lines - spacing,
+                        ],
+                    )
 
-            fig = go.Figure()
-            make_bullet(fig, "Groceries", value=150, budget=200, y_domain=[0.6, 0.8])
-            make_bullet(fig, "Rent", value=2000.17, budget=1820, y_domain=[0.3, 0.5])
+                fig.update_layout(
+                    height=60 * num_lines,
+                    margin=dict(t=30, b=30, l=30, r=30),
+                )
+                ui.plotly(fig)
 
-            fig.update_layout(
-                height=300,
-                margin=dict(t=30, b=30, l=30, r=30),
-            )
-
-            ui.plotly(fig)
+            # render content
+            ui.label("Finance Tracker").classes("text-xl")
+            ui.label("Placeholder for home dashboard")
+            report_div()
